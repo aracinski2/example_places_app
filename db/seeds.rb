5296abe84d849eae42b53849f5b2dd8640b79b17
Alex Racinski <aracinski2@gmail.com>
2020-12-14 19:20:44 -0600

    add places controller, run seed to create data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+places = Place.create(name: "United Center", address: "123 united center drive")
+places = Place.create(name: "wrigley field", address: "42342 chicago ave")
+places = Place.create(name: "sears tower", address: "453 madison st")
+places = Place.create(name: "navy pier", address: "546 street name")
+places = Place.create(name: "UIC", address: "3624 taylor st")
